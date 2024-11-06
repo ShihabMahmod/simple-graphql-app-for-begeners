ea516fa1ab46a26af44c3f382219e4f20038e232
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-11-06 10:44:39 +0600

    Category Brand and product insert and update feature done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name','slug','category_id','brand_id','price','qty'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
